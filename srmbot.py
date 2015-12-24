e4a74b4ed5310155e28275b13ac23d5ef9686151
Arjun mahishi <arjun.mahishi@yahoo.com>
2015-12-24 20:05:42 +0530

    bot script

---
+++ srmbot.py
@@ -0,0 +1,30 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+class newsItem:
+    def getScore(self):
+        score = 0
+        return score
+    def __init__(self,text):
+        self.text = text
+        self.score = self.getScore()
+
+def displayAllNews(news):
+    for i in range(len(news)):
+        print str(i+1) + ". " + news[i].text
+
+def getNews():
+    news = []
+    try:
+        data = urllib2.urlopen("http://www.srmuniv.ac.in").read()
+        soup = BeautifulSoup(data,'html.parser')
+        for tag in soup.find_all('div',{'class':'views-field views-field-title'}):
+            news.append(newsItem(tag.text.strip()))
+    except urllib2.URLError:
+        print "Internet not working"
+    return news
+
+if __name__ == '__main__':
+    news = getNews()
+    displayAllNews(news)
+    raw_input() # hold
