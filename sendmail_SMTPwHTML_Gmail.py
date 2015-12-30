04a1f6fcba8c74f463df0eb85a7d477dfab0e162
Arjun mahishi <arjun.mahishi@yahoo.com>
2015-12-30 12:29:56 +0530

    embedded HTML to email

--- sendmail_SMTPwHTML_Gmail.py
+++ sendmail_SMTPwHTML_Gmail.py
@@ -2,6 +2,22 @@ import smtplib
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.mime.multipart import MIMEMultipart
+import srmbot
+
+def getHTML(newNews):
+        items = ""
+        for i in range(len(newNews)):
+              item = '<a href=\"'+ newNews[i].link + '\"><h4>' + (str(i+1) + '. ' + newNews[i].title) + '</h4></a>\n\t<p>' + newNews[i].snip + '</p>\n\n\n'
+              items += item
+        html = '''
+                <html>
+                <head><h2>News updates</h2></head>
+                <body>
+                %s
+                </body>
+                </html>
+                ''' % items
+        return html
 
 def sendMail(newNews):
         gmail_user = 'srm.news.notifier@gmail.com'
@@ -11,7 +27,7 @@ def sendMail(newNews):
         obj.close()
         FROM = gmail_user
         TO = addr
-        SUBJECT = ''
+        SUBJECT = 'News update'
 # Prepare actual message
         message = MIMEMultipart('alternative')
         message['From'] = FROM
@@ -21,9 +37,10 @@ def sendMail(newNews):
         text = "This is a test message.\nText and html."
         #for i in range(len(newNews)):# contents #
                 #html_data += str(i+1) + ". " + newNews[i].title + newNews[i].link + '\n' + newNews[i].snip + '\n'      
-        load = open("template.html", "r")
+        ''' load = open("template.html", "r")
         data = load.read()
-        html = """ %s """%(data)
+        html = """ %s """%(data)'''
+        html = getHTML(newNews).encode('UTF-8')
 # Record the MIME types of both parts - text/plain and text/html.
         part1 = MIMEText(text, 'plain')
         part2 = MIMEText(html, 'html')
@@ -44,5 +61,4 @@ def sendMail(newNews):
                 print "Unable to send message: %s" % e
 
 if __name__ == '__main__':
-       sendMail()
-       raw_input()
+       #do nothing 
